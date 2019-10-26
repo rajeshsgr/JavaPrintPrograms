a21ccedcb14c0becb906d579d40f8693a2b730d4
Rajesh P <rajeshp@Rajeshs-MacBook-Pro.local>
2019-10-25 21:00:55 -0500

    Added new hour glass pattern

---
+++ src/com/raj/nola/print/HourGlassPattern.java
@@ -0,0 +1,74 @@
+package com.raj.nola.print;
+
+	import java.util.Scanner;
+
+	public class HourGlassPattern {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Please enter the number of rows in Pyramid:");
+			int rows = scanner.nextInt();
+			scanner.close();
+			
+			printUpsideDownPyramid(rows);
+			
+			printPyramidPattern(rows);
+			
+
+		}
+		
+		
+		static void printUpsideDownPyramid(int rows) {
+			
+			// for loop for the rows
+					for (int i = rows; i >= 1; i--) {
+						
+						// white spaces in the front of the numbers
+						int numberOfWhiteSpaces = rows - i;
+						
+						//print leading white spaces
+						printPattern(" ", numberOfWhiteSpaces);
+						
+						
+						printPattern("* ", i);
+
+						//move to next line
+						System.out.println("");
+						
+					}
+			
+
+		}
+		
+		
+		private static void printPyramidPattern(int rows) {
+			
+			// for loop for the rows
+			for (int i = 1; i <= rows; i++) {
+				// white spaces in the front of the numbers
+				int numberOfWhiteSpaces = rows - i;
+
+				//print leading white spaces
+				printPattern(" ", numberOfWhiteSpaces);
+
+				//print *
+				printPattern("* ", i);
+
+				//move to next line
+				System.out.println("");
+				
+			}
+		}
+		
+		
+		
+		private static void printPattern(String pattern, int times) {
+			
+			for (int i = 0; i < times; i++) {
+				System.out.print(pattern);
+			}
+		}
+
+	}
